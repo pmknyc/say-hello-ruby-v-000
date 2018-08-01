fae3c6951417da6176dfae1dc0da8b544c2e6e7f
Paula McKinley <pmknyc@optimum.net>
2018-08-01 21:52:50 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,11 @@
 # Build your say_hello method here
 
-def say_hello (name = "Ruby programmer")
+=begin 
+  Method say_hello prints to console the greeting
+  "Hello <name>!" with a default name of "Ruby Programmer"
+  user can optionally input other name/recipient of greeting
+=end 
+
+def say_hello (name = "Ruby Programmer")
+  puts "Hello #{name}!"
+end
